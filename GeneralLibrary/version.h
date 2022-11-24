a140ecd70c28ce152cdf027b88e4893ca7983298
njit_77 <512424964@qq.com>
2022-11-24 14:32:41 +0800

    feat:1、设置文件编码格式utf-8带签名

--- GeneralLibrary/version.h
+++ GeneralLibrary/version.h
@@ -1,8 +1,8 @@
 //Auto generate file version 
 #define	GeneralLibrary_Major_Version		2022													
-#define	GeneralLibrary_Minor_Version		06													
-#define	GeneralLibrary_Revsion_Version		07													
-#define	GeneralLibrary_RevNum_Version		14													
-#define	GeneralLibrary_Git_Info				"efa2ba118534d5bdf000d0e2c6a732bb4ec3fdd7"													
-#define	GeneralLibrary_Product_Version		"2022.06.07.14"				
-#define	GeneralLibrary_File_Description		"efa2ba1  2022.06.07 / 19:27:55.79"	
+#define	GeneralLibrary_Minor_Version		11													
+#define	GeneralLibrary_Revsion_Version		23													
+#define	GeneralLibrary_RevNum_Version		15													
+#define	GeneralLibrary_Git_Info				"9b3341a8e32fe0db8de27d804c357cf809abe748"													
+#define	GeneralLibrary_Product_Version		"2022.11.23.15"				
+#define	GeneralLibrary_File_Description		"9b3341a  2022.11.23 / 10:28:19.16"	
